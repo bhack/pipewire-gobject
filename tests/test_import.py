d6160ead908ee0dc2481e013fed212bfbfb44944
bhack <bhack@users.noreply.github.com>
2026-05-07 05:03:55 +0200

    Prepare 0.2.0 release

--- tests/test_import.py
+++ tests/test_import.py
@@ -5,7 +5,7 @@ gi.require_version("Pwg", "0.1")
 from gi.repository import GLib, Pwg
 
 Pwg.init()
-assert Pwg.get_library_version() == "0.1.0"
+assert Pwg.get_library_version() == "0.2.0"
 assert isinstance(Pwg.get_pipewire_library_version(), str)
 
 core = Pwg.Core.new()
